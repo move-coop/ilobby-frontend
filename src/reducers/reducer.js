b47e309a0fe27389ff757dbe7b2c900c80a5e8f5
jasmosez <jas.schaffer@gmail.com>
2020-03-24 15:03:24 -0400

    check party filter icons to colors

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -45,13 +45,14 @@ const partyOptions = [
     key: 'Democratic',
     text: 'Democratic',
     value: 'Democratic',
-    icon: "thumbs up"
+    label: { color: 'blue', empty: true, circular: true }
+
   },
   {
     key: 'Republican',
     text: 'Republican',
     value: 'Republican',
-    icon: "thumbs down"
+    label: { color: 'red', empty: true, circular: true }
   }
 ]
 
