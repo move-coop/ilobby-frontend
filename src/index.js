fc5e5055f97afa1c10602030a8fdc9c8b07bf448
jasmosez <jas.schaffer@gmail.com>
2020-08-25 16:54:51 -0400

    install firebase

--- src/index.js
+++ src/index.js
@@ -14,9 +14,24 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk'
 import { reducer } from "./reducers/reducer";
 
+import firebase from 'firebase';
+
+
 const store = createStore(reducer, applyMiddleware(thunk))
 // console.log('store', store)
 // console.log('state', store.getState())
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
   <Provider store={store}>
