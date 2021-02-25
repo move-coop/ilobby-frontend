25b809ca0f4b43fb30f9284e2d41f7301c9ac40a
jamesmorrisschaffer <james.schaffer@datasociety.com>
2021-02-25 00:44:53 -0500

    TMC-1 delete comments and console.log(s)

--- src/index.js
+++ src/index.js
@@ -6,22 +6,13 @@ import {firebaseConfig} from './firebaseConfig'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// IF YOU WISH TO USE REACT ROUTER uncomment lines 7,19,21
 import { BrowserRouter as Router } from 'react-router-dom';
-
-// Provider is a react component which wraps your app and 'provides' your redux store to the rest of your application
 import { Provider } from 'react-redux'
-
-//function which returns store
 import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk'
 import { reducer } from "./reducers/reducer";
 
 const store = createStore(reducer, applyMiddleware(thunk))
-// console.log('store', store)
-// console.log('state', store.getState())
-
-console.log('firebaseConfig', firebaseConfig)
 firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
